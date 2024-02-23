feaf9bf667b87e3b89ea8725d962bd1fcf73989f
Vedansh111 <123576150+Vedansh111@users.noreply.github.com>
2024-02-23 15:22:42 +0530

    completing work...

--- src/Helper Components/InputSettings.js
+++ src/Helper Components/InputSettings.js
@@ -4,7 +4,15 @@ function InputSettings(props) {
     return (
         <div className=' flex p-2 m-2 items-center'>
             <label htmlFor={props.name} className=' text-lg font-semibold mr-3'>{props.title}</label>
-            <input type={props.type} name={props.name} placeholder={props.placeholder} autoComplete='off' id={props.name} value={props.values} onChange={props.handleChange} onBlur={props.handleBlur} className={`p-1 px-2 rounded-md border border-black w-[${props.width}]`} />
+            <input type={props.type}
+                name={props.name}
+                placeholder={props.placeholder}
+                autoComplete='off'
+                id={props.name}
+                value={props.values}
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                className={`p-1 px-2 rounded-md border border-black w-[${props.width}]`} />
             {props.errors && props.touched ? <p className=' text-xs mt-0.5'>{props.errors}</p> : null}
         </div>
     )
